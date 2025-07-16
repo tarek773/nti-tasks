806d7ec577e673210db879bd31339896620e2867
tarek773 <112038444+tarek773@users.noreply.github.com>
2025-07-16 10:58:40 +0300

    Add files via upload

---
+++ Day_3/task_7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 0; $i <= 20; $i+=5) {
+        echo "Number: $i<br>";}
+
+        echo "<br>method 2<br>";
+
+    for ($i = 0; $i <= 20; $i++) {
+        if ($i % 5 == 0) {
+        echo "Number: $i<br>";}}
+    ?>
+</body>
+</html>
